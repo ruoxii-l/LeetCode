7211310d83a4a3f1c3789d12d6f4fde7a94fc644
ruoxii-l <126369751+ruoxii-l@users.noreply.github.com>
2023-03-08 00:22:07 -0800

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,21 @@
+/**
+ * Runtime: 115ms beats 40.63%
+ * Memory: 42.5 MB beats 60.20%
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < (nums.length - 1); i++) {
+        for (let j = i + 1; j <= (nums.length - 1); j++) {
+            let total = nums[i] + nums[j];
+            if (total === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
